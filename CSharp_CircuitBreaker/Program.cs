9c1390bb8891213b4382824c421e46ca0514c21b
Jean Lopes <jean.llopes@outlook.com>
2019-02-04 00:45:26 -0200

    Add project files.

---
+++ CSharp_CircuitBreaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_CircuitBreaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
